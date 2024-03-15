151f973815d276cdd638482e2f759833167fb02f
Vũ Quang Vinh 11 <vinhvq11@fpt.com>
2024-03-15 14:54:29 +0700

    init

---
+++ pdf2img.py
@@ -0,0 +1,47 @@
+import pdf2image
+import argparse
+import logging
+import os
+import fitz  # PyMuPDF
+
+def convert_pdf_to_images(pdf_path, output_folder):
+    # Open the PDF
+    
+    # Close the PDF
+    pdf_document.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--pdf",
+                        required=True,
+                        help="Path to the PDF file")
+    parser.add_argument("--output",
+                        required=False,
+                        default="images",
+                        help="Path to the output folder")
+    parser.add_argument("--format",
+                        required=False,
+                        default="jpg",
+                        help="Output image format")
+
+    args = parser.parse_args()
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+    for file in os.listdir(args.pdf):
+        # Convert the PDF to images
+        pdf_document = fitz.open(os.path.join(args.pdf,file))
+        file = file.replace('.pdf','')
+        # Iterate through each page
+        for page_number in range(len(pdf_document)):
+            # Get the page
+            page = pdf_document.load_page(page_number)
+
+            # Convert the page to an image
+            image = page.get_pixmap()
+
+            # Save the image
+            image.save(os.path.join(
+                    args.output, f"{file}_page_{page_number}.{args.format}"))
+    logging.info(f"PDF converted to images and saved at {args.output}")
