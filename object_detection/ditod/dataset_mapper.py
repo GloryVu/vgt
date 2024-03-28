5e90ef66058f30750b594ec7c22c14fc51c591f0
Vũ Quang Vinh 11 <vinhvq11@fpt.com>
2024-03-28 10:32:23 +0700

    test

--- object_detection/ditod/dataset_mapper.py
+++ object_detection/ditod/dataset_mapper.py
@@ -131,17 +131,16 @@ class DetrDatasetMapper:
         
         # try:s
         name = dataset_dict["file_name"][0:-4].split('/') 
-        print(f'---------------------{name}-----------------')
-        if 'publaynet' in name:
+        # if 'publaynet' in name:
             
-            root = '/'.join(name[:-2])
-            if name[-2] == 'val':
-                name[-2] = 'dev'
-            pdf_name = '/'.join(['/VGT_publaynet_grid_pkl'] + name[-1:])
-            with open((root + pdf_name + '.pdf.pkl'), "rb") as f:
-                sample_inputs = pickle.load(f)
-            input_ids = sample_inputs["input_ids"]
-            bbox_subword_list = sample_inputs["bbox_subword_list"]
+        root = '/'.join(name[:-2])
+        if name[-2] == 'val':
+            name[-2] = 'dev'
+        pdf_name = '/'.join(['/VGT_publaynet_grid_pkl'] + name[-1:])
+        with open((root + pdf_name + '.pdf.pkl'), "rb") as f:
+            sample_inputs = pickle.load(f)
+        input_ids = sample_inputs["input_ids"]
+        bbox_subword_list = sample_inputs["bbox_subword_list"]
         # elif 'DocBank' in name:
         #     root = '/'.join(name[:-2])
         #     pdf_name = '/'.join(['/VGT_docbank_grid_pkl'] + name[-1:])
